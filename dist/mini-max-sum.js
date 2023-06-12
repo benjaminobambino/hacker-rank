12ef848bb14b99437b2f05cdab91ac7202300148
Benjamin Peck <benjaminlpeck@gmail.com>
2023-06-12 12:26:46 -0700

    compile

--- dist/mini-max-sum.js
+++ dist/mini-max-sum.js
@@ -1,20 +1,3 @@
-'use strict';
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-let inputString = '';
-let inputLines = [];
-let currentLine = 0;
-process.stdin.on('data', function (inputStdin) {
-    inputString += inputStdin;
-});
-process.stdin.on('end', function () {
-    inputLines = inputString.split('\n');
-    inputString = '';
-    main();
-});
-function readLine() {
-    return inputLines[currentLine++];
-}
 /*
  * Complete the 'miniMaxSum' function below.
  *
@@ -27,9 +10,5 @@ function miniMaxSum(arr) {
     const minSum = totalSum - arr[arr.length - 1];
     const maxSum = totalSum - arr[0];
     console.log(minSum + " " + maxSum);
-}
-function main() {
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    miniMaxSum(arr);
 }
 //# sourceMappingURL=mini-max-sum.js.map
